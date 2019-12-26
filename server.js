d252921bab015a84892a8b86d391f81ee39f34df
Maxim Hollingworth <maximhollingworth@fuqua.attlocal.net>
2019-12-26 12:05:22 -0600

    dog text suggestion

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var server = Connect.createServer(
 	Connect.logger(), // Log responses to the terminal using Common Log Format.
 	//Connect.responseTime(), // Add a special header with timing information.
 	Connect.conditionalGet(), // Add HTTP 304 responses to save even more bandwidth.
-	Connect.cache(), // Add a short-term ram-cache to improve performance.
+	Connect.cache(), // dog Add a short-term ram-cache to improve performance.
 	Connect.gzip(), // Gzip the output stream when the browser wants it.
 	Connect.staticProvider(__dirname) // Serve all static files in the current dir.
 );
